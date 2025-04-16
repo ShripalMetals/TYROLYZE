7cb3d498976ec88d8e4986a14e81045d604160e5
Aryan Ojha <aryannojha1@gmail.com>
2025-04-16 23:50:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+import ComingSoon from "./components/ComingSoon";
+
+function App() {
+  return (
+  <ComingSoon />
+  );
+}
+
+export default App;
---
+++ src/components/ComingSoon.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./ComingSoon.css";
+
+const ComingSoon = () => {
+  return (
+    <div className="coming-soon-container">
+      <div className="left-section">
+        <div className="logo1">
+          <img src="/image/Tyre.jpg" alt="Tyrolyze Logo" className="logo-img" />
+        </div>
+        <h2 className="logo12"><b>Tyrolyze</b></h2>
+        <h1 className="logo12">Coming Soon...</h1>
+      </div>
+    </div>
+  );
+};
+
+export default ComingSoon;
